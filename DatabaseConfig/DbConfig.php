282c6d27bfba3c48ecf113f00e8a6db0f35065db
LiamFrance <57929913+LiamFrance@users.noreply.github.com>
2021-03-30 19:41:48 +0700

    a
    a

--- DatabaseConfig/DbConfig.php
+++ DatabaseConfig/DbConfig.php
@@ -1,7 +1,7 @@
 <?php
-$servername="localhost";
-$DBusername="root";
-$DBpass="root";
+$servername="ec2-34-252-251-16.eu-west-1.compute.amazonaws.com";
+$DBusername="ykjsyxshexqjyz";
+$DBpass="a41db76161b02937b3222bc1d675aaeba4ecbe2e1132a88cdd6f33cc8698281a";
 $DBname="comp1640";
 
 $conn= mysqli_connect($servername,$DBusername,$DBpass,$DBname);
